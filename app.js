82cc66fa02b1c3ffd91e7cc8778ee3a62456e2e5
Julian So <ch.julian.so@gmail.com>
2021-08-06 21:50:57 -0400

    finished edit.hbs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
@@ -23,13 +24,31 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate, editIcon, select } = require("./helpers/hbs"); // {} means destructuring
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs"); // {} means destructuring
 
 // Handlebars
 app.engine(
